47557c4bbf081eb75513e1dabc5516ba204ec21f
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2026-03-09 21:30:13 +0300

    подключение бд и создание таблицы

---
+++ backend/src/db.ts
@@ -0,0 +1,33 @@
+import sqlite3 from "sqlite3";
+import { open, Database } from "sqlite";
+
+const PATH = './db.db'
+
+let db: Database<sqlite3.Database, sqlite3.Statement>;
+async function initDb() {
+    try{
+        console.log();
+        db = await open({
+            filename: PATH,
+            driver: sqlite3.Database
+        })
+        db.run(`
+            CREATE TABLE IF NOT EXISTS employees(
+                id INTEGER PRIMARY KEY AUTOINCREMENT
+                birth_date TEXT
+                passport_data TEXT
+                contact TEXT
+                adress TEXT 
+                job TEXT 
+                department TEXT 
+                salary INTEGER 
+                hire_date TEXT
+            );
+        `)
+    }catch(err){
+        console.log("Ошибка создания бд");
+        process.exit(1)
+    }
+}
+
+export {db, initDb}
