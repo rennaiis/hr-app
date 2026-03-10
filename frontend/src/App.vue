820702215a33c15d8a426b49a23abda13d5f3208
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2026-03-10 18:20:41 +0300

    HTML для формы добавления нового сотрудника

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,89 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
+  import departments from './data/departments.json'
+  import jobs from './data/jobs.json'
+  const name = ref('')
+  const birth_date = ref(new Date())
+  const passport_data = ref('')
+  const contact = ref('')
+  const adress = ref('')
+  const departmentChosen = ref('')
+  const jobChosen = ref('')
+  const salary = ref(0)
+  const hire_date = ref(new Date())
+
 </script>
 
 <template>
   <div>
-
+    <h1>Сотрудники</h1>
+    <div>
+      <form action="">
+        <input type="text" id="search">
+        <label for="seatch"><img src="" alt=""></label>
+        <button type="submit">Найти</button>
+      </form>
+      <button>+</button>
+    </div>
+    <form action="">
+      <h3>Новый сотрудник</h3>
+      <p>
+        <label for="name">ФИО</label>
+        <input 
+          v-model="name" 
+          type="text" 
+          id="name">
+      </p>
+      <p>
+        <label for="birth_date">Дата рождения</label>
+        <input 
+          type="date" 
+          id="birth_date"
+          v-model="birth_date">
+      </p>
+      <p>
+        <label for="passport_data">Серия и номер паспорта</label>
+        <input 
+          v-model="passport_data"
+          v-maska="'#### ######'"
+          type="text"
+          id="passport_data"
+          placeholder="0000 000000"
+          imputmode="numeric">
+      </p>
+      <p>
+        <label for="contact">Телефон</label>
+        <input 
+          v-model="contact"
+          v-maska="'+#(###)###-##-##'"
+          type="tel"
+          id="contact"
+          placeholder="+7(000)000-00-00"
+          imputmode="tel">
+      </p>
+      <p>
+        <label for="adress">Адрес проживания</label>
+        <input 
+          v-model="adress" 
+          type="text" 
+          id="adress">
+      </p>
+      
+      <p>
+        <label for="salary">Зарплата</label>
+        <input 
+          v-model="salary" 
+          type="number" 
+          id="salary">
+      </p>
+      <p>
+        <label for="hire_date">Дата приёма на работу</label>
+        <input 
+          v-model = "hire_date"
+          type="date" 
+          id="hire_date">
+      </p>
+    </form>
   </div>
 </template>
 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { vMaska } from 'maska/vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.directive('maska', vMaska)
+app.mount('#app')
