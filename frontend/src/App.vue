c3f632921d32d9bac4070240813e7166c4a605d3
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2026-03-13 01:27:06 +0300

    стили для формредактирования и добавления

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -120,87 +120,91 @@
         <button @click="openAddForm" v-if="!isAddFormOpen">Добавить</button>
         <button @click="openAddForm"  v-if="isAddFormOpen">Скрыть</button>
       </div>
-    
-    <form @submit ="submitAdd" v-if="isAddFormOpen" class="block form-block">
-      <h3>Новый сотрудник</h3>
-      <p class="input-item">
-        <label for="name">ФИО</label>
-        <input 
-          v-model="newEmployee.name" 
-          type="text" 
-          id="name">
-      </p>
-      <p class="input-item">
-        <label for="birth_date">Дата рождения</label>
-        <input 
-          type="date" 
-          id="birth_date"
-          v-model="newEmployee.birthDate">
-      </p>
-      <p class="input-item">
-        <label for="passport_data">Серия и номер паспорта</label>
-        <input 
-          v-model="newEmployee.passportData"
-          v-maska="'#### ######'"
-          type="text"
-          id="passport_data"
-          placeholder="0000 000000"
-          imputmode="numeric">
-      </p>
-      <p class="input-item">
-        <label for="contact">Телефон</label>
-        <input 
-          v-model="newEmployee.contact"
-          v-maska="'+#(###)###-##-##'"
-          type="tel"
-          id="contact"
-          placeholder="+7(000)000-00-00"
-          imputmode="tel">
-      </p>
-      <p class="input-item">
-        <label for="adress">Адрес проживания</label>
-        <input 
-          v-model="newEmployee.adress" 
-          type="text" 
-          id="adress">
-      </p>
-      <p class="input-item">
-        <label for="department">Отдел</label>
-        <select 
-          id="department"
-          v-model="newEmployee.department">
-          <option v-for="department in departments" :key="department" :value="department">
-            {{ department }}
+    <div class="overlay" v-if="isAddFormOpen">
+      <form @submit ="submitAdd" v-if="isAddFormOpen" class="block form-block">
+        <h3>Новый сотрудник</h3>
+        <p class="input-item">
+          <label for="name">ФИО</label>
+          <input 
+            v-model="newEmployee.name" 
+            type="text" 
+            id="name">
+        </p>
+        <p class="input-item">
+          <label for="birth_date">Дата рождения</label>
+          <input 
+            type="date" 
+            id="birth_date"
+            v-model="newEmployee.birthDate">
+        </p>
+        <p class="input-item">
+          <label for="passport_data">Серия и номер паспорта</label>
+          <input 
+            v-model="newEmployee.passportData"
+            v-maska="'#### ######'"
+            type="text"
+            id="passport_data"
+            placeholder="0000 000000"
+            imputmode="numeric">
+        </p>
+        <p class="input-item">
+          <label for="contact">Телефон</label>
+          <input 
+            v-model="newEmployee.contact"
+            v-maska="'+#(###)###-##-##'"
+            type="tel"
+            id="contact"
+            placeholder="+7(000)000-00-00"
+            imputmode="tel">
+        </p>
+        <p class="input-item">
+          <label for="adress">Адрес проживания</label>
+          <input 
+            v-model="newEmployee.adress" 
+            type="text" 
+            id="adress">
+        </p>
+        <p class="input-item">
+          <label for="department">Отдел</label>
+          <select 
+            id="department"
+            v-model="newEmployee.department">
+            <option v-for="department in departments" :key="department" :value="department">
+              {{ department }}
+            </option>
+          </select>
+        </p>
+        <p class="input-item">
+          <label for="job">Профессия</label>
+          <select 
+            id="job"
+            v-model="newEmployee.job"
+          >
+          <option v-for="job in jobs" :key="job" :value="job">
+            {{ job }}
           </option>
         </select>
-      </p>
-      <p class="input-item">
-        <label for="job">Профессия</label>
-        <select 
-          id="job"
-          v-model="newEmployee.job"
-        >
-        <option v-for="job in jobs" :key="job" :value="job">
-          {{ job }}
-        </option>
-      </select>
-      </p>
-      <p class="input-item">
-        <label for="salary">Зарплата</label>
-        <input 
-          v-model="newEmployee.salary" 
-          type="number" 
-          id="salary">
-      </p>
-      <p class="input-item">
-        <label for="hire_date">Дата приёма на работу</label>
-        <input 
-          v-model = "newEmployee.hireDate"
-          type="date" 
-          id="hire_date">
-      </p>
-      <button type="submit">Добавить</button>
+        </p>
+        <p class="input-item">
+          <label for="salary">Зарплата</label>
+          <input 
+            v-model="newEmployee.salary" 
+            type="number" 
+            id="salary">
+        </p>
+        <p class="input-item">
+          <label for="hire_date">Дата приёма на работу</label>
+          <input 
+            v-model = "newEmployee.hireDate"
+            type="date" 
+            id="hire_date">
+        </p>
+        <div class="inline">
+          <button type="submit">Добавить</button>
+          <button @click="openAddForm">Отмена</button>
+        </div>
     </form>
+    </div>
     <div class="table-block">
       <table v-if="isTableEmpty">
         <thead>
--- frontend/src/EditWindow.vue
+++ frontend/src/EditWindow.vue
@@ -31,86 +31,92 @@
   
 </script>
 <template>
+    
     <div v-if="visible && copyChosenEmployee">
-        <form>
-            <h3>Редактирование сотрудника</h3>
-            <p>
-                <label for="name">ФИО</label>
-                <input 
-                    v-model="copyChosenEmployee.name"
-                    type="text" 
-                    id="name">
-            </p>
-            <p>
-                <label for="birth_date">Дата рождения</label>
-                <input 
-                    v-model="copyChosenEmployee.birthDate"
-                    type="date" 
-                    id="birth_date">
-            </p>
-            <p>
-                <label for="passport_data">Серия и номер паспорта</label>
-                <input 
-                    v-model="copyChosenEmployee.passportData"
-                    v-maska="'#### ######'"
-                    type="text"
-                    id="passport_data"
-                    placeholder="0000 000000"
-                    imputmode="numeric">
-            </p>
-            <p>
-                <label for="contact">Телефон</label>
-                <input 
-                    v-model="copyChosenEmployee.contact"
-                    v-maska="'+#(###)###-##-##'"
-                    type="tel"
-                    id="contact"
-                    placeholder="+7(000)000-00-00"
-                    imputmode="tel">
-            </p>
-            <p>
-                <label for="adress">Адрес проживания</label>
-                <input 
-                    v-model="copyChosenEmployee.adress"
-                    type="text" 
-                    id="adress">
-            </p>
-            <p>
-                <label for="department">Отдел</label>
-                <select 
-                    v-model="copyChosenEmployee.department"
-                    id="department">
-                    <option v-for="department in departments" :key="department" :value="department">
-                        {{ department }}
+        <div class="overlay">
+            <form class="block form-block">
+                <h3>Редактирование сотрудника</h3>
+                <p class="input-item">
+                    <label for="name">ФИО</label>
+                    <input 
+                        v-model="copyChosenEmployee.name"
+                        type="text" 
+                        id="name">
+                </p>
+                <p class="input-item">
+                    <label for="birth_date">Дата рождения</label>
+                    <input 
+                        v-model="copyChosenEmployee.birthDate"
+                        type="date" 
+                        id="birth_date">
+                </p>
+                <p class="input-item">
+                    <label for="passport_data">Серия и номер паспорта</label>
+                    <input 
+                        v-model="copyChosenEmployee.passportData"
+                        v-maska="'#### ######'"
+                        type="text"
+                        id="passport_data"
+                        placeholder="0000 000000"
+                        imputmode="numeric">
+                </p>
+                <p class="input-item">
+                    <label for="contact">Телефон</label>
+                    <input 
+                        v-model="copyChosenEmployee.contact"
+                        v-maska="'+#(###)###-##-##'"
+                        type="tel"
+                        id="contact"
+                        placeholder="+7(000)000-00-00"
+                        imputmode="tel">
+                </p>
+                <p class="input-item">
+                    <label for="adress">Адрес проживания</label>
+                    <input 
+                        v-model="copyChosenEmployee.adress"
+                        type="text" 
+                        id="adress">
+                </p>
+                <p class="input-item">
+                    <label for="department">Отдел</label>
+                    <select 
+                        v-model="copyChosenEmployee.department"
+                        id="department">
+                        <option v-for="department in departments" :key="department" :value="department">
+                            {{ department }}
+                        </option>
+                    </select>
+                </p>
+                <p class="input-item">
+                    <label for="job">Профессия</label>
+                    <select 
+                        v-model="copyChosenEmployee.job"
+                        id="job">
+                    <option v-for="job in jobs" :key="job" :value="job">
+                        {{ job }}
                     </option>
                 </select>
-            </p>
-            <p>
-                <label for="job">Профессия</label>
-                <select 
-                    v-model="copyChosenEmployee.job"
-                    id="job">
-                <option v-for="job in jobs" :key="job" :value="job">
-                    {{ job }}
-                </option>
-            </select>
-            </p>
-            <p>
-                <label for="salary">Зарплата</label>
-                <input 
-                    v-model="copyChosenEmployee.salary"
-                    type="number" 
-                    id="salary">
-            </p>
-            <p>
-                <label for="hire_date">Дата приёма на работу</label>
-                <input 
-                    v-model="copyChosenEmployee.hireDate"
-                    type="date" 
-                    id="hire_date">
-            </p>
-            <button @click="handleSave">Сохранить</button>
-            <button @click="handleClose">Отмена</button>
-        </form>
+                </p>
+                <p class="input-item">
+                    <label for="salary">Зарплата</label>
+                    <input 
+                        v-model="copyChosenEmployee.salary"
+                        type="number" 
+                        id="salary">
+                </p>
+                <p class="input-item">
+                    <label for="hire_date">Дата приёма на работу</label>
+                    <input 
+                        v-model="copyChosenEmployee.hireDate"
+                        type="date" 
+                        id="hire_date">
+                </p>
+                <div class="inline">
+                    <button @click="handleSave">Сохранить</button>
+                    <button @click="handleClose">Отмена</button>
+                </div>
+                
+            </form>
+        </div>
     </div>
 </template>
